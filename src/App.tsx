2e2e0c2dc891c41fe62beed85bfe0af02ce6a5d7
MaximBaltak <maksimbaltak@yandex.ru>
2022-03-24 20:32:35 +0200

    add project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import svgIcons from './img/icons.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <svg width={100} height={100}>
+          <use xlinkHref={`${svgIcons}#arrow`}/>
+        </svg>
     </div>
   );
 }
